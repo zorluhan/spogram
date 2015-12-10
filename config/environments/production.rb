023e8cd776e20af0d3bd8dc530eb0c33b119569b
zorluhan zorlu <zorluhan@gmail.com>
2015-12-10 16:38:45 +0200

    congi.assets.precompile added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,9 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.assets.precompile += %w( application.css )
+   config.assets.precompile += %w( application.js )
+   config.assets.precompile += %w(bootstrap_and_customization.css.scss)
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
