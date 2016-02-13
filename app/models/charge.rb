9dfc6f1a4a04d67e725426c5bab823d3179c7fad
Hassan Ahmed <hassan.a.murtaza@gmail.com>
2016-02-13 22:05:43 +0500

    added email notifications

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -2,4 +2,10 @@ class Charge < ActiveRecord::Base
   belongs_to :instauser
   belongs_to :branduser
 
+  after_create :send_new_proposal_email
+
+  private
+    def send_new_proposal_email
+      UserMailer.send_new_proposal_email(instauser_id, branduser_id, id).deliver!
+    end
 end
--- app/models/instauser.rb
+++ app/models/instauser.rb
@@ -11,4 +11,17 @@ class Instauser < ActiveRecord::Base
 
   serialize :recent_media_urls
 
+  after_create :send_welcome_email
+
+
+  private
+    def send_welcome_email
+      if followed_by >= 5000
+        # to send emails in delayed jobs use
+        # UserMailer.instauser_welcome_email(id).delay.deliver!
+        UserMailer.instauser_welcome_email(id).deliver!
+      else
+        UserMailer.instauser_reject_email(id).deliver!
+      end
+    end
 end
