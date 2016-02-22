8f5dc4ad975bd748bf287525731cc08a7393df89
zorluhan zorlu <zorluhan@gmail.com>
2016-02-22 13:58:05 +0200

    removed send_welcome_mail from private

--- app/models/instauser.rb
+++ app/models/instauser.rb
@@ -14,6 +14,21 @@ class Instauser < ActiveRecord::Base
 
   #after_create :send_welcome_email
 
+  def send_welcome_email
+      if Rails.env.production?
+        if email.present?
+        if followed_by >= 5000
+          # to send emails in delayed jobs use
+          # UserMailer.instauser_welcome_email(id).delay.deliver!
+          UserMailer.instauser_welcome_email(id).deliver!
+        else
+          UserMailer.instauser_reject_email(id).deliver!
+        end
+      end
+      end      
+    end
+    
+
   def fetch_instagram_timeline
     if access_token.present?
 
@@ -84,17 +99,5 @@ class Instauser < ActiveRecord::Base
 
 
   private
-    def send_welcome_email
-      if Rails.env.production?
-        if email.present?
-        if followed_by >= 5000
-          # to send emails in delayed jobs use
-          # UserMailer.instauser_welcome_email(id).delay.deliver!
-          UserMailer.instauser_welcome_email(id).deliver!
-        else
-          UserMailer.instauser_reject_email(id).deliver!
-        end
-      end
-      end      
-    end
+    
 end
