b1b070a438c1ecb4cf1c69a051e19cddef37df60
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-02 16:13:54 +0200

    removed jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.require jquery.spin
 // require spin
 
-//= require jquery
+// require jquery
 //= require jquery_ujs
 //= require jquery.min.js
 //= require jquery-ui.min.js
