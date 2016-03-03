cb140416fe066f432e26b09ad340bf4bc40d1438
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-03 23:20:01 +0200

    fixing burger with js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
   });
 });
 
-$(document).on('click','.navbar-collapse.in',function(e) {
+$(document).on('click','#bs-example-navbar-collapse-1.in',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
     }
