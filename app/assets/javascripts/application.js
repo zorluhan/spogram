9a2354129a45c8042fbbca2d946c1e59f6808b6b
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-03 20:02:25 +0200

    addition to application js to fix hamburger

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,6 +50,10 @@ $(document).ready(function() {
   });
 });
 
-
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
 
 
