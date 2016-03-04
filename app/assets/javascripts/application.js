57d2e73ca31f16c9f1ef403844b133674b9ecf48
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-04 15:19:18 +0200

    home page navbar duzeltıldı

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,5 +50,15 @@ $(document).ready(function() {
   });
 });
 
- 
+$(document).ready(function() {
+    var text_max = 99;
+    $('#textarea_feedback').html(text_max + ' characters remaining');
+
+    $('#textarea').keyup(function() {
+        var text_length = $('#textarea').val().length;
+        var text_remaining = text_max - text_length;
+
+        $('#textarea_feedback').html(text_remaining + ' characters remaining');
+    });
+});
  
