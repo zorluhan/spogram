0d04e7ded8634a000369722b5f3189b275f5ab0a
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-06 18:38:57 +0200

    signup form revised

--- app/models/instauser.rb
+++ app/models/instauser.rb
@@ -7,15 +7,15 @@ class Instauser < ActiveRecord::Base
   has_many :brandusers, through: :charges
   has_many :instaposts
 
-  validates :location , presence: true 
-  validates :date_of_birth, presence: true
-  validates :email, presence: true 
-  validates :email, :email_format => {:message => 'is not looking good'}
-  validates :firstname, presence: :true 
-  validates :lastname, presence: :true 
-  validates :theme , presence: true 
-  validates :gender, presence: true 
-  validates :bio, length: { maximum: 300 }
+ # validates :location , presence: true 
+ # validates :date_of_birth, presence: true
+  #validates :email, presence: true 
+ # validates :email, :email_format => {:message => 'is not looking good'}
+ # validates :firstname, presence: :true 
+ # validates :lastname, presence: :true 
+  #validates :theme , presence: true 
+  #validates :gender, presence: true 
+  #validates :bio, length: { maximum: 300 }
 
 
   #include Elasticsearch::Model
