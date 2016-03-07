29fa1be03a2812fb2e4a8d8e0b1be82b7cd6d81d
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-07 14:36:29 +0200

    turned off validation in model

--- app/models/instauser.rb
+++ app/models/instauser.rb
@@ -12,7 +12,7 @@ class Instauser < ActiveRecord::Base
  # validates :date_of_birth, presence: true
   #validates :email, presence: true 
  # validates :email, :email_format => {:message => 'is not looking good'}
-  validates :firstname, presence: :true
+ # validates :firstname, presence: :true
  #validates :lastname, presence: :true,  :if => :first_time_user?   
   #validates :theme , presence: true 
   #validates :gender, presence: true 
