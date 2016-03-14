f76c7df985211049ff750b5621abc54396c89283
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-14 16:42:37 +0200

    messages sorted at list controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -68,11 +68,11 @@ class MessagesController < ApplicationController
   def list 
     if current_instauser 
       @branduser = Branduser.find_by_id(params[:branduser_id])
-      @messages  = Message.where(branduser_id: @branduser.id, instauser_id: current_instauser.id)
+      @messages  = Message.where(branduser_id: @branduser.id, instauser_id: current_instauser.id).sort_by(&:created_at)
       @messages.where(is_read: false, sender: 0).update_all(is_read: true)
     elsif current_branduser 
       @instauser = Instauser.find_by_id(params[:instauser_id])
-      @messages  = Message.where(instauser_id: @instauser.id, branduser_id: current_branduser.id)
+      @messages  = Message.where(instauser_id: @instauser.id, branduser_id: current_branduser.id).sort_by(&:created_at)
       @messages.where(is_read: false, sender: 1).update_all(is_read: true)
     end
   end
