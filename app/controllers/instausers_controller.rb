ec3e9f89829b84b52348a2f86e01e0530b993366
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-20 11:34:59 +0200

    search functionality shared search box and integer check for followers)

--- app/controllers/instausers_controller.rb
+++ app/controllers/instausers_controller.rb
@@ -100,7 +100,15 @@ end
 
   def search
    #@instausers=Instauser.where(:theme => params[:instauser][:theme]).where(:gender => params[:instauser][:gender]).where(:age => params[:instauser][:age1]..params[:instauser][:age2]).where("followed_by >=?", params[:instauser][:followed_by]).where("postprice <?", params[:instauser][:postprice]).where("averagelikes >=?", params[:instauser][:averagelikes]).where(:disabled=> false)  if !params[:instauser].nil?  
+if params[:instauser][:followed_by]=="" 
+  flash!(:error => "Write min. number of followers you are looking for")  
+  redirect_to "/bdashboard"
+elsif !(params[:instauser][:followed_by].to_i.to_s==params[:instauser][:followed_by])
+   flash!(:error => "Please enter a number")  
+  redirect_to "/bdashboard"
+else
  @instausers=Instauser.where(:disabled => false).where(:theme => params[:instauser][:theme]).where("followed_by >=?", params[:instauser][:followed_by]).where(:gender => params[:instauser][:gender])
+end
  
 end
 
