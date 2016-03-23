d17cba253ef57c367375e0ec312de50c4c42a575
Zorluhan Zorlu <zorluhan@gmail.com>
2016-03-23 18:05:35 +0200

    order desc featured instausers

--- app/controllers/brandusers_controller.rb
+++ app/controllers/brandusers_controller.rb
@@ -26,7 +26,7 @@ class BrandusersController < ApplicationController
 
   def dashboard
     @branduser= Branduser.find_by_id(session[:buser_id])
-    @instausers=Instauser.where(:disabled=>false).order("followed_by ASC").take(12)
+    @instausers=Instauser.where(:disabled=>false).order("followed_by DESC").take(12)
 
   end
 
