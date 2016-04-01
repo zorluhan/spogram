42be879bfb6d00d4f0318a9e53063f218b01cb51
Zorluhan Zorlu <zorluhan@gmail.com>
2016-04-01 06:57:12 +0300

    proposal fixed

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -32,7 +32,7 @@ class ChargesController < ApplicationController
       @escrowed_spending = current_branduser.escrowed_spending
       @total_spending = current_branduser.total_spending
     elsif instauser_logged_in?
-      @charges            = current_instauser.charges..where(state: "pending")
+      @charges            = current_instauser.charges.where(state: "pending")
       @potential_earnings = current_instauser.potential_earnings
       @upcomming_eatnings = current_instauser.upcomming_eatnings
       @total_earnings     = current_instauser.total_earnings
