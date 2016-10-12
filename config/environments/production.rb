6a1f2ca93772a9b68ac466041d98727e87f057e3
Nitin <nitin.barai777@gmail.com>
2016-10-12 18:24:28 +0530

    smpt port chagne for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,10 +102,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:        'smtp.gmail.com',
     port:           587,
-    domain:         'smtp.gmail.com',
+    domain:         'gmail.com',
     # user_name:      'mail@capish.co',
     # password:       'gjlpatqzqfjvnjsg',
-    user_name:      'rahulvi.dev@gmail.com',
+    user_name:      'rahulvi.dev',
     password:       'Shelter#2016',
     authentication: 'login',
     enable_starttls_auto: true
