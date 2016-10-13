7621414eb490788f106e1c49f3cdce57cbf690b7
Nitin <nitin.barai777@gmail.com>
2016-10-13 13:36:24 +0530

    Chaneg in config variable for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
     address:        'smtp.gmail.com',
     port:           587,
     domain:         'gmail.com',
-    user_name:      ENV["EMAIL"],
-    password:       ENV["PASSWORD"],
+    user_name:      ENV["GMAIL_SMTP_USER"],
+    password:       ENV["GMAIL_SMTP_PASSWORD"],
     authentication: 'plain',
     :enable_starttls_auto  => true
   }
