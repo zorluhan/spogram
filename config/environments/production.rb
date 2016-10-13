c8a121a85fda7ed6e417a2138c24e1aa4f8fa8b6
Nitin <nitin.barai777@gmail.com>
2016-10-13 20:01:21 +0530

    Revert changes to original

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
     address:        'smtp.gmail.com',
     port:           587,
     domain:         'gmail.com',
-    user_name:      'rahulvi.dev',
-    password:       'Shelter#2016',
+    user_name:      'mail@capish.co',
+    password:       'gjlpatqzqfjvnjsg',
     authentication: 'plain',
     :enable_starttls_auto  => true
   }
