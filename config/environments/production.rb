71ccb64d37f586c3522bf5a1b299d9eec09bcdd5
Nitin <nitin.barai777@gmail.com>
2016-10-14 13:11:12 +0530

    revert chantges

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,10 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = true
   
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -80,11 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'capish.co' }
-
-  config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'cryptic-mountain-3688.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
@@ -103,8 +103,8 @@ Rails.application.configure do
     address:        'smtp.gmail.com',
     port:           587,
     domain:         'gmail.com',
-    user_name:      'rahulvi.dev',
-    password:       'Shelter#2016',
+    user_name:      'mail@capish.co',
+    password:       'gjlpatqzqfjvnjsg',
     authentication: 'plain',
     :enable_starttls_auto  => true
   }
