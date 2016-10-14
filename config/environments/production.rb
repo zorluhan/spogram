5ddb417af8236a97c1f66fc7d4130717f0f7455e
Nitin <nitin.barai777@gmail.com>
2016-10-14 11:01:48 +0530

    Changed username and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
@@ -103,8 +103,8 @@ Rails.application.configure do
     address:        'smtp.gmail.com',
     port:           587,
     domain:         'gmail.com',
-    user_name:      'mail@capish.co',
-    password:       'gjlpatqzqfjvnjsg',
+    user_name:      'rahulvi.dev',
+    password:       'Shelter#2016',
     authentication: 'plain',
     :enable_starttls_auto  => true
   }
