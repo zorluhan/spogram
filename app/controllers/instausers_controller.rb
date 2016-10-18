bf45c5b9c52e3d8d2c57e7d70bc13907f40efef0
Nitin <nitin.barai777@gmail.com>
2016-10-18 10:38:40 +0530

    Removed debug code from instauser controller

--- app/controllers/instausers_controller.rb
+++ app/controllers/instausers_controller.rb
@@ -77,9 +77,7 @@ class InstausersController < ApplicationController
     if @instauser.update(instauser_params)
       session[:instauser_id] = nil
       instauser_log_in(@instauser)
-      p "============user #{instauser_log_in(@instauser).inspect}"
       @instauser.send_welcome_email
-      p "==================after mail send====="
       redirect_to "/dashboard"
     else
 
