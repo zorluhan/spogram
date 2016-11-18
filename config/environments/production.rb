15bb87162b13c65f172c24912e97ad609080299c
Zorluhan Zorlu <zorluhan@gmail.com>
2016-11-18 16:49:44 +0200

    zz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
  
   config.action_mailer.smtp_settings = {
     address:        'smtp.gmail.com',
-    port:           587,
+    port:           465,
     domain:         'gmail.com',
     user_name:      'mail@capish.co',
     password:       'gjlpatqzqfjvnjsg',
